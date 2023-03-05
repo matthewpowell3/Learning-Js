7817541f0e4905c9e4e5fa0266f26617047c7f6b
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 10:27:03 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.write('Hello Matthew');
+// let a;
+// let b;
+
+// a = 3;
+// b = 5;
+
+// document.write();
+
+// var name;
+// name = 'Matthew';
+// document.write('Hello ' + name);
+
+// var name;
+// name = 'Mark';
+// document.write(name);
+
+// var can let you redefined variables
+
+// let name;
+// name = 'Matthew';
+// document.write('Hello ' + name);
+
+// let name;
+// name = 'Mark';
+// document.write(name);
+
+// let does not make you redefine a variable with the same name
+
